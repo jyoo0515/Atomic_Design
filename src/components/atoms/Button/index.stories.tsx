c7709a6361b5f94061f707f3651e64e2bccf55de
jyoo0515 <jasonyoo0515@gmail.com>
2022-03-06 21:06:08 +0900

    fix: property matching

--- src/components/atoms/Button/index.stories.tsx
+++ src/components/atoms/Button/index.stories.tsx
@@ -15,23 +15,15 @@ const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 export const Primary = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 Primary.args = {
-  primary: true,
-  label: 'Hello',
+  type: 'button',
 };
 
-export const Secondary = Template.bind({});
-Secondary.args = {
-  label: 'Button',
+export const Reset = Template.bind({});
+Reset.args = {
+  type: 'reset',
 };
 
-export const Large = Template.bind({});
-Large.args = {
-  size: 'large',
-  label: 'Button',
-};
-
-export const Small = Template.bind({});
-Small.args = {
-  size: 'small',
-  label: 'Button',
+export const Submit = Template.bind({});
+Submit.args = {
+  type: 'submit',
 };
